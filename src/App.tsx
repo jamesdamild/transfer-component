c0cf8a979715091b4bb436655ea9de9539fb45ea
james.etienne <damild82@gmail.com>
2024-10-11 12:05:05 -0400

    Feat: added and remove fonctionaly to transfer component, save and reset functionality , searching input list.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,157 @@
-import { Button, Col, Divider, Row } from "antd";
+import { Button, Col, Divider, Modal, Row } from "antd";
 import { PersonsList } from "./components/PersonsList";
 import { persons_can_be_added, persons_can_be_removed } from "./DataSet2";
+//import { persons_can_be_added, persons_can_be_removed } from "./DataSet";
 import styled from "styled-components";
+import { useState } from "react";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [personsToBeAdded, addPerson] =
+    useState<unknown[]>(persons_can_be_added);
+  const [personsToBeRemoved, removePerson] = useState<unknown[]>(
+    persons_can_be_removed
+  );
+  const [filterPersonsToBeAdded, setFilterAddPerson] = useState<any[]>([]);
+  const [filterPersonsToBeRemoved, setFilterRemovePerson] = useState<any[]>([]);
+
+  /*  to allow dynamic keys in component */
+  const displayKeys = ["displayName", "informations"];
+  const uniqueKey="person_id";
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const addOrRemovePerson = (person: any, actionType: string) => {
+    /* Here we do a deepCopy of the object to prevent it changing original object  */
+    const deepCopyPerson = JSON.parse(JSON.stringify(person));
+    if (actionType === "add") {
+      let filterPersonToBeAdded = personsToBeAdded.filter(
+        (data: any) => data[uniqueKey] !== person[uniqueKey]
+      );
+      deepCopyPerson.color = "#e9efeb";
+      removePerson([...personsToBeRemoved, deepCopyPerson]);
+      addPerson([...filterPersonToBeAdded]);
+    } else {
+      let filterPersonToBeRemoved = personsToBeRemoved.filter(
+        (data: any) => data[uniqueKey] !== person[uniqueKey]
+      );
+      deepCopyPerson.color = "#fee3e2";
+      addPerson([...personsToBeAdded, deepCopyPerson]);
+      removePerson([...filterPersonToBeRemoved]);
+    }
+  };
+
+  const handleSetFilteredData = (actionType: string, value: string) => {
+    if (actionType === "add") {
+      const filterAddPerson = value
+        ? personsToBeAdded.filter((person: any) =>
+            person[displayKeys[0]].toLowerCase().includes(value.toLowerCase())
+          )
+        : [];
+      setFilterAddPerson(filterAddPerson);
+    } else {
+      const filterRemovePerson = value
+        ? personsToBeRemoved.filter((person: any) =>
+            person[displayKeys[0]].toLowerCase().includes(value.toLowerCase())
+          )
+        : [];
+      setFilterRemovePerson(filterRemovePerson);
+    }
+  };
+
+  const handleReset = () => {
+    addPerson(persons_can_be_added);
+    removePerson(persons_can_be_removed);
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const showAddRemovedPersons = () => {
+    setLoading(true);
+    setTimeout(() => {
+      const filteredAddPeople = personsToBeAdded.filter((person) => {
+        return (
+          typeof person === "object" && person !== null && "color" in person
+        );
+      });
+
+      const filteredRemovePeople = personsToBeRemoved.filter((person) => {
+        return (
+          typeof person === "object" && person !== null && "color" in person
+        );
+      });
+      console.log(
+        `Items Added:${JSON.stringify(
+          filteredAddPeople
+        )} and Items removed: ${JSON.stringify(filteredRemovePeople)}`
+      );
+      setLoading(false);
+    }, 3000); // 3 seconds delay
+  };
+
   return (
     <StyledContainer>
       <Row gutter={16}>
         <Col span={12}>
-          <PersonsList title="Can be Added" persons={persons_can_be_added} />
+          <PersonsList
+            title="Can be Added"
+            persons={
+              filterPersonsToBeAdded.length > 0
+                ? filterPersonsToBeAdded
+                : personsToBeAdded
+            }
+            actionType="add"
+            transferPerson={addOrRemovePerson}
+            setFilteredData={handleSetFilteredData}
+            displayKeys={displayKeys}
+            uniqueKey={uniqueKey}
+          />
         </Col>
         <Col span={12}>
           <PersonsList
             title="Can be Removed"
-            persons={persons_can_be_removed}
+            persons={
+              filterPersonsToBeRemoved.length > 0
+                ? filterPersonsToBeRemoved
+                : personsToBeRemoved
+            }
+            actionType="remove"
+            transferPerson={addOrRemovePerson}
+            setFilteredData={handleSetFilteredData}
+            displayKeys={displayKeys}
+            uniqueKey={uniqueKey}
           />
         </Col>
       </Row>
       <Divider />
       <Row justify="end" gutter={8}>
-        {/* Action buttons */}
-        <Button size="large" type="link">
+        <Button size="large" type="link" onClick={showModal}>
           Reset
         </Button>
-        <Button size="large" type="primary">
+        <Button
+          loading={loading}
+          size="large"
+          type="primary"
+          onClick={showAddRemovedPersons}
+        >
           Save
         </Button>
       </Row>
+
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleReset}
+        okText="Reset"
+        onCancel={handleCancel}
+        cancelText="Cancel"
+      >
+        <p>Do you want to reset the data?</p>
+      </Modal>
     </StyledContainer>
   );
 }
--- src/components/PersonsList.tsx
+++ src/components/PersonsList.tsx
@@ -1,27 +1,60 @@
 import { Button, Col, Divider, Input, Row, Typography } from "antd";
+import { debounce } from 'lodash'; 
 
-// Types and components can be adjusted at will
-export const PersonsList = ({
+/* Types and components can be adjusted at will
+ Generic type T for dynamic types*/
+
+ interface PersonsListProps<T> {
+  persons: T[];
+  title: string;
+  transferPerson: (person: T, type: string) => void;
+  setFilteredData: (type: string, value: string) => void;
+  actionType: string;
+  displayKeys: (keyof T)[]; // Array of keys to display
+  uniqueKey: keyof T; // Unique identifier key for person_id or id or else '
+}
+
+export const PersonsList = <T extends object>({
   persons,
   title,
-}: {
-  persons: any;
-  title: any;
-}) => {
+  transferPerson,
+  setFilteredData,
+  actionType,
+  displayKeys,
+  uniqueKey
+}: PersonsListProps<T>) => {
+  
+   /*Here we pass the person  that has to be add or remove 
+     to the parent component
+   */ 
+  const handlePersonClick=(person:T)=>{
+    transferPerson(person,actionType)
+  }
+
+  /*Here we use a debounce function from lodash package so we can delay the filtering 
+    while the user types in the search bar
+  */ 
+  const handleSearchPerson = debounce((value:string) => {
+     setFilteredData(actionType,value);
+   }, 600);
+  
   return (
     <div>
       <Typography.Title level={2}>{title}</Typography.Title>
       <Divider />
-      <Input.Search placeholder="Search by name" size="large" />
+      <Input.Search placeholder="Search by name" size="large"  onChange={(e)=>handleSearchPerson(e.target.value)}/>
       {persons.map((person: any) => (
-        <Row justify="space-between" align="middle">
-          <Col flex="1">
-            <h3>{person.name}</h3>
-            <p>{person.description}</p>
+        <Row justify="space-between" align="middle" key={person[uniqueKey]} style={{backgroundColor:person.color}}>
+          <Col flex="1">      
+          {displayKeys.map((key,index:number) => (
+            /*Here we set  dynamic keys so this component can accepts any proprties name */
+            index===0?<h3 key={key.toString()}>{person[key]}</h3>:<p key={key.toString()}>{person[key]}</p>
+            
+          ))}      
           </Col>
           <Col flex="120px">
             {/* Add or Remove Action */}
-            <Button>Action</Button>
+            <Button onClick={()=>handlePersonClick(person)}>Action</Button>
           </Col>
         </Row>
       ))}
